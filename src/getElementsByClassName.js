8e58db68c7f17fcc005349980b547145cb01c015
qhenkart <qhenkart@gmail.com>
2015-02-24 15:58:29 -0800

    solved getElementsByClassName WAHOO and finally learned how to use chrome devtools debugging

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,25 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className, child, elements){
+	if (child == undefined) {
+		var elements = [];
+		if (document.body.classList[0] == className) elements.push(document.body);
+		child = document.body.childNodes;
+	}
+
+	for (var i = 0; i < child.length; i++){
+		if (child[i].nodeType === 1){
+
+			for (var y=0; y< child[i].classList.length; y++){
+				if (child[i].classList[y] == className){
+					elements.push(child[i])
+				}
+			}
+			getElementsByClassName(className, child[i].childNodes, elements)
+		
+		}
+	}
+
+	return elements;
 };
