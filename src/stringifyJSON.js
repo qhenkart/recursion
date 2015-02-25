eae9ac14a056bf887703f483e64469597c9af12f
qhenkart <qhenkart@gmail.com>
2015-02-24 18:37:30 -0800

    Finished stringify in exactly 3 hours!!! Now finished with precourse!!!! Wahooooooooooooooo

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,7 +2,38 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
+'use strict';
+var stringifyJSON = function(obj, stringified){
 
-var stringifyJSON = function(obj) {
-  // your code goes here
+	if (stringified === undefined) stringified = '';
+	if (obj === null || typeof obj === 'number' || typeof obj === 'boolean' ){
+		stringified += obj;
+	}else if (typeof obj === 'string') {
+		stringified += '"' + obj + '"';
+	}else if (obj.constructor === Array){
+		stringified += '[';
+		obj.forEach(function(x,index){
+			if (index > 0 && index < obj.length) stringified+=',';
+			stringified += stringifyJSON(x);
+			//stringified += ',';
+		});
+		stringified += ']';
+	}else if (obj.constructor === Object){
+		stringified += '{';
+		var counter = 0;
+		for (var key in obj){
+			if (obj[key] === undefined || typeof obj[key] === 'function') {
+				return "{}";
+			}
+
+			var temp = Object.keys(obj);
+			if (counter > 0 && counter < temp.length) stringified +=',';
+			stringified += '"' +key +'"'+ ':';
+			stringified += stringifyJSON(obj[key]);
+			//stringified += ',';
+			counter ++;
+		}
+		stringified +='}';
+	}
+	return stringified;
 };
